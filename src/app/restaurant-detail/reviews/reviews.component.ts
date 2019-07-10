d5b56eab87401374ce2ed69d37d842a39240107a
Elton Lemes <elton.silva@resourceit.com>
2019-07-10 09:58:57 -0300

    Avaliacao flexivel

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
@@ -9,12 +9,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ReviewsComponent implements OnInit {
 
-  reviews: Observable<any>
+  reviews: Observable<any>;
+  avaliacao: any;
 
   constructor(private restaurantService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.reviews = this.restaurantService.reviewsOfRestaurant(this.route.parent.snapshot.params['id']);
+    this.avaliacao = 5;
+  }
+
+  calcula_avaliacao(nota) {
+    var porcentagem = (nota * 100)/this.avaliacao;
+    var img = "assets/img/reactions/";
+
+    if(porcentagem > 50) {
+      img = img + "loved.png";
+    }
+
+    if(porcentagem == 50) {
+      img = img + "soso.png";
+    }
+
+    if(porcentagem < 50) {
+      img = img + "pissed.png";
+    }
+    
+    return img;
   }
 
 }
